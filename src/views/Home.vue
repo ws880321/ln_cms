05a61e3b5ac52c86891f02a02c29744be573c500
ws880321 <wangshuai@vip.126.com>
2020-03-29 19:35:11 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <el-container>
+   <el-header>辽宁项目</el-header>
+   <el-main>Main</el-main>
+ </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
